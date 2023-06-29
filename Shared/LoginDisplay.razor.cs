2800b6c39a0d6fd36cb9ed686f61eb2b8548867f
Edafe Iduh <edafe.iduh@flex.com>
2023-06-28 17:07:51 -0700

    First Push

---
+++ Shared/LoginDisplay.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MasterEL_Web.Shared
+{
+    public partial class LoginDisplay
+    {
+        [Inject] public NavigationManager Navigation { get; set; }
+
+        [Parameter] public string ReturnUrl { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ReturnUrl = Navigation.ToBaseRelativePath(Navigation.Uri);
+        }
+
+    }
+}
